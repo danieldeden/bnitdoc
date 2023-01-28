30ce6375cfb1e7a70f9fd755f32b508c2adf801e
victor.forsgren <victor.forsgren@bnearit.se>
2023-01-28 22:33:51 +0100

    Update layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,34 +9,19 @@ import SpacesFeatures from '@site/src/components/SpacesFeatures';
 
 import styles from './index.module.css';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/spaces">
-            Show all Spaces
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
-
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
       <main className="container container--fluid margin-vert--lg">
         <SpacesFeatures />
+        <Link
+            className="button button--secondary button--lg"
+            to="/spaces">
+            Show all Spaces
+          </Link>
       </main>
     </Layout>
   );
